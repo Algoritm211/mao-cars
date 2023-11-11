2dde4cc786d7dc84de80264187f7ce88875a6984
Alexey <algoritm211@gmail.com>
2023-11-11 13:26:48 +0200

    feature: add setup for react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import Layout from "@/system/layout/layout";
+import {useState} from "react";
+import {HydrationBoundary, QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </HydrationBoundary>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
