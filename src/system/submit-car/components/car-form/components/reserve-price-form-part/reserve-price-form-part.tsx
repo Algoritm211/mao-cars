69c0701c1494ffc4f1d1969c251900391232429e
Alexey <algoritm211@gmail.com>
2023-12-25 19:36:37 +0200

    feat: add reserve price translation

--- src/system/submit-car/components/car-form/components/reserve-price-form-part/reserve-price-form-part.tsx
+++ src/system/submit-car/components/car-form/components/reserve-price-form-part/reserve-price-form-part.tsx
@@ -1,26 +1,25 @@
+import { useTranslations } from 'next-intl';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { FormBlockWrapper, Input } from '@/shared/components/form';
 
 export const ReservePriceFormPart = () => {
+  const t = useTranslations('Car_submit_page.form.reserve_price');
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
   return (
-    <FormBlockWrapper title="Reserve Price">
-      <p className="bg-gray-200 p-4 m-2 rounded-xl my-4">
-        The reserve price is a secret, minimum price required for your vehicle to sell. Cars with
-        reserve prices may garner less interest than those without reserves.
-      </p>
+    <FormBlockWrapper title={t('section_title')}>
+      <p className="bg-gray-200 p-4 m-2 rounded-xl my-4">{t('reserve_price_description')}</p>
       <div className="form-control max-w-xl">
         <Input
           id="reservePrice"
-          label="What reserve price would you like (USD)?"
+          label={t('reserve_price_label')}
           type="number"
-          placeholder="Please enter your reserve price"
+          placeholder={t('reserve_price_placeholder')}
           register={register}
           errors={errors}
           registerOptions={{ valueAsNumber: true, value: 0, required: true }}
