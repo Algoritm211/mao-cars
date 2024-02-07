379f1201bb1b58727ba527ddf91007645c4e373e
Alexey <algoritm211@gmail.com>
2024-02-08 00:45:38 +0200

    chore: add price block and buttons

--- src/system/auction-details/components/report-section/report-section.tsx
+++ src/system/auction-details/components/report-section/report-section.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 
 import { reportSections } from '@/system/auction-details/components/report-section/models/report-sections';
 
-import { Icon } from '@/shared/components';
+import { button, Icon } from '@/shared/components';
+import { currencyFormatter } from '@/shared/utils';
 
 const getIcon = (isPositive: boolean) => {
   return (
@@ -24,25 +25,43 @@ export const ReportSection = () => {
   return (
     <React.Fragment>
       <div className="divider px-1">Special offer</div>
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto text-center">
         <table className="table">
           <thead>
             <tr>
               <th></th>
-              <th className="text-center">VIN +</th>
-              <th className="text-center">VIN Max</th>
+              <th className="text-center text-lg">VIN +</th>
+              <th className="text-center text-lg">VIN Max</th>
             </tr>
           </thead>
           <tbody>
             {reportSections.map((reportElem) => (
               <tr key={reportElem.labelKey}>
-                <th>{t(reportElem.labelKey)}</th>
+                <th className="text-lg">{t(reportElem.labelKey)}</th>
                 <td className="text-center">{getIcon(reportElem.isAvailablePlus)}</td>
                 <td className="text-center">{getIcon(reportElem.isAvailableMax)}</td>
               </tr>
             ))}
+            <tr className="text-green-600">
+              <th className="text-lg">{t('price')}</th>
+              <td className="text-center text-xl">{currencyFormatter(20)}</td>
+              <td className="text-center text-xl">{currencyFormatter(40)}</td>
+            </tr>
+            <tr className="hidden md:table-row">
+              <th></th>
+              <td className="text-center">
+                <button className={button()}>{t('buy_vin_plus')}</button>
+              </td>
+              <td className="text-center">
+                <button className={button()}>{t('buy_vin_max')}</button>
+              </td>
+            </tr>
           </tbody>
         </table>
+      </div>
+      <div className="flex gap-2 my-2 md:hidden">
+        <button className={button({ className: 'flex-grow' })}>{t('buy_vin_plus')}</button>
+        <button className={button({ className: 'flex-grow' })}>{t('buy_vin_max')}</button>
       </div>
       <div className="divider px-1">Check your car</div>
     </React.Fragment>
