2dccf1bf7697169994d50642848d47a49292d05c
Alexey <algoritm211@gmail.com>
2024-02-16 23:44:46 +0200

    fix: title

--- src/system/submit-car/components/sell-car-preview/components/head-title/head-title.tsx
+++ src/system/submit-car/components/sell-car-preview/components/head-title/head-title.tsx
@@ -6,7 +6,7 @@ export const HeadTitle = () => {
     'block w-fit relative after:block after:absolute after:w-full after:border after:rounded after:bg-purple-300 after:h-2 after:-z-10 after:bottom-0.5';
   return (
     <div className="mb-10">
-      <h1 className={clsx('text-4xl font-bold mb-3', textAccent)}>Sell on MaoCars!</h1>
+      <h1 className={clsx('text-4xl font-bold mb-3', textAccent)}>Sell on Mao Cars!</h1>
       <h2 className="text-3xl font-bold mb-3">More money, fewer headaches</h2>
     </div>
   );
